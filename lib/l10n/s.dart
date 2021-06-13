f20635d910b1276d834dc9ac753a6c824fcd33ef
ccolorcat <ch.xx@outlook.com>
2021-06-13 16:37:23 +0800

    feat(*): add i18n support

---
+++ lib/l10n/s.dart
@@ -0,0 +1,18 @@
+// Author: cxx
+// Date: 2021-06-13
+// GitHub: https://github.com/ccolorcat
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class S {
+  static Iterable<LocalizationsDelegate<dynamic>> get localizationsDelegates =>
+      AppLocalizations.localizationsDelegates;
+
+  static Iterable<Locale> get supportedLocales =>
+      AppLocalizations.supportedLocales;
+
+  static AppLocalizations of(BuildContext context) {
+    return AppLocalizations.of(context)!;
+  }
+}
