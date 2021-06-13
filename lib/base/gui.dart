aea443be3221710ac244e47fc5b5229f31537196
ccolorcat <ch.xx@outlook.com>
2021-06-13 17:11:24 +0800

    feat(*): add gui.dart

---
+++ lib/base/gui.dart
@@ -0,0 +1,7 @@
+// Author: cxx
+// Date: 2021-06-13
+// GitHub: https://github.com/ccolorcat
+
+export 'package:flutter/material.dart';
+export 'package:flutter_mobx/flutter_mobx.dart' show Observer;
+export '../l10n/s.dart';
