37b213f85843572d6cb2dfe65baafd59869567c2
ccolorcat <ch.xx@outlook.com>
2021-06-14 20:28:56 +0800

    feat(gui.dart`): add field and export

--- lib/base/gui.dart
+++ lib/base/gui.dart
@@ -3,5 +3,11 @@
 // GitHub: https://github.com/ccolorcat
 
 export 'package:flutter/material.dart';
+export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_mobx/flutter_mobx.dart' show Observer;
+
 export '../l10n/s.dart';
+
+typedef ValueCallback<T> = void Function(T value);
+
+const double KMargin = 8.0;
