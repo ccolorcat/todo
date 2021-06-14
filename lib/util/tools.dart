ff2bbab7c04a55a682e3ce3ebd1be584a7377816
ccolorcat <ch.xx@outlook.com>
2021-06-14 23:30:30 +0800

    feat: add showEditDialog function

--- lib/util/tools.dart
+++ lib/util/tools.dart
@@ -3,9 +3,15 @@
 // GitHub: https://github.com/ccolorcat
 
 import 'package:intl/intl.dart';
+import 'package:todo/base/gui.dart';
+import 'package:todo/widget/edit_dialog.dart';
 
 final _dateFormatter = DateFormat('M/dd h:mm');
 
 String formatDateTime(DateTime dateTime) {
   return _dateFormatter.format(dateTime);
+}
+
+Future<String?> showEditDialog(BuildContext context) {
+  return showDialog<String>(context: context, builder: (_) => EditDialog());
 }
