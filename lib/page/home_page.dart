a5f1f27644215f9421a222e1ad6b7ef2feda06d3
ccolorcat <ch.xx@outlook.com>
2021-06-14 23:39:52 +0800

    feat: add task list

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,11 +2,55 @@
 // Date: 2021-06-13
 // GitHub: https://github.com/ccolorcat
 import "package:todo/base/gui.dart";
+import 'package:todo/data/task.dart';
+import 'package:todo/data/task_dao.dart';
+import 'package:todo/data/task_repository.dart';
+import 'package:todo/page/home_store.dart';
+import 'package:todo/util/tools.dart';
+import 'package:todo/widget/task_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
 
-  void _handleCreate() {}
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ScrollController? controller;
+  final HomeStore store = HomeStore(TaskRepository(TaskDao()));
+
+  @override
+  void initState() {
+    super.initState();
+    controller = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  void _handleCreate(BuildContext context) {
+    showEditDialog(context).then((value) {
+      if (value?.isNotEmpty == true) {
+        store.addTask(value!);
+      }
+    });
+  }
+
+  void _handleComplete(Task task) {
+    store.complete(task);
+  }
+
+  void _handleIncomplete(Task task) {
+    store.incomplete(task);
+  }
+
+  void _handleDelete(Task task) {
+    store.delete(task);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +59,24 @@ class HomePage extends StatelessWidget {
         title: Text(S.of(context).title),
         centerTitle: true,
       ),
-      body: Center(
-        child: const Text('todo list'),
+      body: Observer(
+        builder: (_) {
+          final tasks = store.tasks;
+          return ListView.builder(
+            key: PageStorageKey('tasks'),
+            itemBuilder: (_, index) => TaskWidget(
+              task: tasks[index],
+              onComplete: _handleComplete,
+              onIncomplete: _handleIncomplete,
+              onDelete: _handleDelete,
+            ),
+            itemCount: tasks.length,
+            controller: controller,
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _handleCreate,
-        tooltip: 'add',
+        onPressed: () => _handleCreate(context),
         child: Icon(Icons.add),
       ),
     );
