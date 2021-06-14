2098dfe21c00b6c3320ecf408e479f95a5c92957
ccolorcat <ch.xx@outlook.com>
2021-06-14 23:30:06 +0800

    feat: add edit_dialog.dart

---
+++ lib/widget/edit_dialog.dart
@@ -0,0 +1,62 @@
+// Author: cxx
+// Date: 2021-06-14
+// GitHub: https://github.com/ccolorcat
+
+import 'package:todo/base/gui.dart';
+
+class EditDialog extends StatefulWidget {
+  EditDialog({Key? key}) : super(key: key);
+
+  @override
+  State<EditDialog> createState() => _EditDialogState();
+}
+
+class _EditDialogState extends State<EditDialog> {
+  TextEditingController? controller;
+  String? text;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TextEditingController()
+      ..addListener(() {
+        this.text = controller?.text;
+      });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final local = S.of(context);
+    final actions = [
+      TextButton(
+        child: Text(local.cancel),
+        onPressed: () {
+          Navigator.of(context).pop(null);
+        },
+      ),
+      TextButton(
+        child: Text(local.confirm),
+        onPressed: () {
+          Navigator.of(context).pop(text);
+        },
+      )
+    ];
+    return AlertDialog(
+      content: TextField(
+        textInputAction: TextInputAction.done,
+        maxLines: null,
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: local.inputTip,
+        ),
+      ),
+      actions: actions,
+    );
+  }
+}
