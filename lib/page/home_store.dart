285486a04853fd53ca198083d41890c33ba81039
ccolorcat <ch.xx@outlook.com>
2021-06-14 23:39:09 +0800

    refactor: add log to HomeStore

--- lib/page/home_store.dart
+++ lib/page/home_store.dart
@@ -5,12 +5,15 @@ import 'package:todo/base/vm.dart';
 import 'package:todo/data/category.dart';
 import 'package:todo/data/task.dart';
 import 'package:todo/data/task_repository.dart';
+import 'package:todo/util/log.dart';
 
 part 'home_store.g.dart';
 
 class HomeStore = _HomeStore with _$HomeStore;
 
 abstract class _HomeStore with Store {
+  final _log = Log('HomeStore');
+
   _HomeStore(this.taskRepo) {
     _updateTasks();
   }
@@ -29,21 +32,25 @@ abstract class _HomeStore with Store {
   @action
   void addTask(String content) {
     final task = Task.create(_category.name, content);
+    _log.ld(() => 'addTask: $task');
     taskRepo.insert(task).then((value) => _updateTasks());
   }
 
   @action
   void complete(Task task) {
+    _log.lv(() => 'complete: $task');
     taskRepo.update(task..complete()).then((value) => _updateTasks());
   }
 
   @action
   void incomplete(Task task) {
+    _log.lv(() => 'incomplete: $task');
     taskRepo.update(task..incomplete()).then((value) => _updateTasks());
   }
 
   @action
   void delete(Task task) {
+    _log.li(() => 'delete: $task');
     taskRepo.delete(task).then((value) => _updateTasks());
   }
 
